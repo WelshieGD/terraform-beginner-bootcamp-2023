776dc19815bacb2c2706705e89b0d5f2149d30aa
WelshieGD <120795390+WelshieGD@users.noreply.github.com>
2023-09-24 14:03:09 +0100

    17 restructure root module (#18)
    * Update week1.md
    
    * Update
    
    * update
    
    * added AWS
    
    * Add Terraform Cloud Variables

--- main.tf
+++ main.tf
@@ -8,27 +8,6 @@ terraform {
   }
 }
 
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
@@ -40,11 +19,8 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
  tags = {
-   Name        = "My bucket"
-   Environment = "Dev"
+    UserUuid = var.user_uuid
  }
 }
 
-output "random_bucket_name_result" {
-  value = random_string.bucket_name.result
-}
+
