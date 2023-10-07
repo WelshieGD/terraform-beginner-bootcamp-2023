befa24415be5b8f9a2c9713bc161ac55047681e4
Graham Davies <graham@grahamdavies.info>
2023-10-07 15:02:35 +0000

    41 - Deploy to Terratowns

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-## Commented out for branch 35 - Terraform Provider 
-# output "bucket_name" {
-#   description = "Bucket Name"
-#   value = module.terrahouse_aws.bucket_name
-# }
+# Commented out for branch 35 - Terraform Provider 
+output "bucket_name" {
+  description = "Bucket Name"
+  value = module.terrahouse_aws.bucket_name
+}
 
-# output "s3_website_endpoint" {
-#   description = "S3 Static Website Host Endpoint"
-#   value = module.terrahouse_aws.website_endpoint 
-# }
+output "s3_website_endpoint" {
+  description = "S3 Static Website Host Endpoint"
+  value = module.terrahouse_aws.website_endpoint 
+}
 
-# output "cloudfront_url" {
-#   description = "The CloudFront Distribution Domain Name"
-#   value = module.terrahouse_aws.cloudfront_url
-# }
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
 
